b047dcb5a42446ca5f5e9e2a42677274cee22b9b
Doidel <romanbolzern@hotmail.com>
2016-02-15 16:21:15 +0100

    hut texture corrections, worker hunter setup

---
+++ Assets/model/Buildings/HuntingShack.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HuntingShack : IGameBuilding
+{
+
+    public override float ProductionOutput
+    {
+        get;
+        protected set;
+    }
+
+    private UnityAction listener;
+
+    // Available items that can be built
+    private List<BuildItem> buildItems = new List<BuildItem>();
+    public override BuildItem[] Items
+    {
+        get
+        {
+            return buildItems.ToArray();
+        }
+
+        protected set
+        {
+            buildItems = new List<BuildItem>(value);
+        }
+    }
+
+    public override Tile Location
+    {
+        get;
+        set;
+    }
+
+    public override BuildOrder Producing
+    {
+        get;
+        protected set;
+    }
+
+    public override void Produce(BuildItem item)
+    {
+        Producing = new BuildOrder(item, 0);
+        TimeManager.instance.NoMoreOrdersNeeded(this);
+        Debug.Log("Producing now " + item.Title);
+    }
+
+    public override void Select()
+    {
+        base.Select();
+        audioSource.PlayOneShot(GameManager.instance.Select1, 0.2f);
+        BuildingPanelUI.instance.SetBuildItems(buildItems.ToArray(), (int)ProductionOutput);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        buildItems.Add(GameManager.instance.AvailableBuildItems.First());
+        buildItems.Add(GameManager.instance.AvailableBuildItems.First(f => f.Title == "Animal Husbandry"));
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        listener = new UnityAction(NextRound);
+        ProductionOutput = 10;
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("NextRound", listener);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("NextRound", listener);
+    }
+
+    void NextRound()
+    {
+        if (Producing != null)
+        {
+            Producing.Produced += ProductionOutput;
+            // is the item completed?
+            if (Producing.Produced >= Producing.Item.ProductionCosts)
+            {
+                if (Producing.Item.Produces is ResearchItem)
+                {
+                    GameManager.instance.Research.FinishedResearching(Producing.Item.Title);
+                }
+                else
+                {
+                    GameObject produces = (GameObject)Producing.Item.Produces;
+                    var movement = produces.GetComponent<CharacterMovement>();
+                    if (movement != null)
+                    {
+                        GridManager.instance.Spawn(produces, new Vector2(Location.X + Location.Y / 2, Location.Y));
+                    }
+                }
+                Producing = null;
+                TimeManager.instance.NeedNewOrders(this);
+            }
+
+            // update the building display if this building is currently selected
+            if (GridManager.instance.selectedBuilding == gameObject)
+                BuildingPanelUI.instance.SetCurrentlyBuilding();
+        }
+    }
+
+    public override bool NeedsOrders()
+    {
+        return Producing == null;
+    }
+}
---
+++ Assets/model/Units/Worker.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Worker : IGameUnit {
+
+
+    public GameObject VillagePrefab;
+    public Sprite icon_expand;
+    public AudioClip ExpandAudioClip;
+
+    private static String[] actions = new String[]
+    {
+        "Expand"
+    };
+
+    public override string[] Actions
+    {
+        get
+        {
+            return actions;
+        }
+
+        protected set
+        {
+            Worker.actions = value;
+        }
+    }
+
+    protected CharacterMovement movement;
+
+    public override void UseAction(int action)
+    {
+        if (action == 0)
+        {
+            if (movement.ExpendMovementPoints(2))
+            {
+                GameObject village = Instantiate(VillagePrefab);
+                village.transform.position = transform.position;
+                var gb = village.GetComponent<IGameBuilding>();
+                gb.Location = movement.curTile;
+                TimeManager.instance.NoMoreOrdersNeeded(this);
+                RemoveCharacter();
+                gb.audioSource.PlayOneShot(ExpandAudioClip, 1f);
+                LeaveAction(_settlementHexArea);
+            }
+        }
+    }
+
+    public override void Select()
+    {
+        base.Select();
+        // draw own panel
+        UnitPanelUI.instance.SetUnitPanelInfo(new UnitPanelUI.UnitInfo[] {
+            new UnitPanelUI.UnitInfo(icon_expand, "testtooltip")
+        });
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        movement = GetComponent<CharacterMovement>();
+        movement.MovementPointsMax = 2;
+    }
+
+    void Update()
+    {
+        if (_settlementHexArea > -1 && _settlementHexAreaPos != movement.curTile)
+        {
+            GridManager.instance.DrawHexArea(_settlementHexAreaPos, 2, 0, Tile.TileColorPresets.WhiteTransparent);
+            HoverAction(_settlementHexArea);
+        }
+    }
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    private int _settlementHexArea = -1;
+    private Tile _settlementHexAreaPos = null;
+
+    public override void HoverAction(int action)
+    {
+        GridManager.instance.DrawHexArea(movement.curTile, 2, 1, Tile.TileColorPresets.Area);
+        _settlementHexArea = action;
+        _settlementHexAreaPos = movement.curTile;
+    }
+
+    public override void LeaveAction(int action)
+    {
+        GridManager.instance.DrawHexArea(movement.curTile, 2, 0, Tile.TileColorPresets.WhiteTransparent);
+        _settlementHexArea = -1;
+    }
+
+    public override bool NeedsOrders()
+    {
+        if (movement.RemainingPath.Count > 1 || movement.MovementPointsRemaining == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
